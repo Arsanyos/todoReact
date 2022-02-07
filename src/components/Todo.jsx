bcdb15a33f6a8246b6a7483a2487c131c113c2ec
abenihab <abenih785181@gmail.com>
2022-02-07 09:33:05 -0800

    created todo component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todo = () => {
+
+    return (
+        <div className="todo">
+            <li className="todo container"}>"todo container"</li>
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+        </div>
+    );
+}
+export default Todo;
+
