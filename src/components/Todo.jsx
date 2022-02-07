5f537c9cc61a0988d247bcca43dc4200333b818c
abenihab <abenih785181@gmail.com>
2022-02-07 13:28:28 -0800

    updated components of todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Todo = ({ text, todo, todos, setTodos }) => {
+<<<<<<< Updated upstream
+=======
+ const deleteHandler = (e) => {
+        //it deletes when the el.id is equal to the todo id(WHEN THE CONDITION IS FALSE)
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    }
+>>>>>>> Stashed changes
     const completeHandler = () => {
         setTodos(todos.map((item) => {
             if (item.id === todo.id) {
@@ -18,6 +25,12 @@ const Todo = ({ text, todo, todos, setTodos }) => {
             <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
+<<<<<<< Updated upstream
+=======
+             <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+>>>>>>> Stashed changes
            
         </div>
     );
