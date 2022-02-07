ee2878c59a09be6f9a5d5bcfff9872b0cba36019
abenihab <abenih785181@gmail.com>
2022-02-07 13:05:14 -0800

    updated components of todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-const Todo = () => {
-
+const Todo = ({ text, todo, todos, setTodos }) => {
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+ 
     return (
         <div className="todo">
-            <li className="todo container"}>"todo container"</li>
-            <button className="complete-btn">
+            <li className="todo container"}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
+           
         </div>
     );
 }
 export default Todo;
-
