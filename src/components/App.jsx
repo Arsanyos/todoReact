aa52fed63505f3f2491c74860a6abeaf5f880976
Arsanyos <arsanyos1600@gmail.com>
2022-02-07 23:13:43 +0300

    App.jsx status tracker added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+//importing components
+import Form from './Form';
+import TodoList from './TodoList';
 const App = () => {
+    //states
+    const [inputText, setInputText] = useState(" ");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState("all");
+ 
     return (
         <div className="App">
             <header>
                 <h1>Todo List</h1>
             </header>
+            <Form setStatus={setStatus} inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
+            <TodoList todos={todos} setTodos={setTodos} />
         </div>
     );
 }
 export default App;
-
-
