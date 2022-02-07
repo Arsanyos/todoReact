562f792c5113b7fe6df6dc2af2e3f11c0f906e43
Arsanyos <arsanyos1600@gmail.com>
2022-02-07 23:45:04 +0300

    filter handler and use effect added to App.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //importing components
 import Form from './Form';
 import TodoList from './TodoList';
 const App = () => {
+//function
+    const filterHandler = () => {
+        switch (status) {
+            case 'completed':
+                setFilteredTodos(todos.filter(todo => todo.completed === true));
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter(todo => todo.completed === false));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    }
     //states
     const [inputText, setInputText] = useState(" ");
     const [todos, setTodos] = useState([]);
     const [status, setStatus] = useState("all");
+    const [filteredTodos, setFilteredTodos] = useState([]);
+    //USE EFFECT
+    useEffect(() => {
+        filterHandler();
+    }, [todos, status]);
  
     return (
         <div className="App">
@@ -14,7 +33,7 @@ const App = () => {
                 <h1>Todo List</h1>
             </header>
             <Form setStatus={setStatus} inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
-            <TodoList todos={todos} setTodos={setTodos} />
+            <TodoList todos={todos} filteredTodos={filteredTodos} setTodos={setTodos} />
         </div>
     );
 }
