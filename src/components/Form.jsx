7d5f1ba0f78fc83b0690c128aab4ab681fc5c457
Abraham <aberhamtesfaye@gmail.com>
2022-02-08 12:15:08 +0300

    Form.jsx has been created

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Form(){
+    return(
+        <div>
+            <form>
+                <input type="text" className="todo-input" id="todo-input"/>
+                <button onClick="" className="todo-button" type="submit">
+                    
+                </button>
+            </form>
+        </div>
+    );
+}
