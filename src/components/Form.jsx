238cca3259ddc837fe37fb139dae74f6589defad
Abraham <aberhamtesfaye@gmail.com>
2022-02-08 12:52:02 +0300

    inputText handler and submit handler created for Form.jsx component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+//we can write javascript code and function
 
-function Form(){
-    return(
-        <div>
-            <form>
-                <input type="text" className="todo-input" id="todo-input"/>
-                <button onClick="" className="todo-button" type="submit">
-                    
-                </button>
-            </form>
-        </div>
+const Form = () => {
+
+    return (
+    <div>
+        <form>
+            <input onChange={inputTextHandler} value={inputText} type="text" className="todo-input" id="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+        </form>
+     </div>
     );
 }
+export default Form;
