45ada7ea34e71cd984fd942596ae0f1e2ad34275
Benat <abenezermarkos14@gmail.com>
2022-02-08 10:49:09 +0300

    comments deleted

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,8 +5,6 @@ const TodoList = ({ filteredTodos, setTodos, todos }) => {
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                {/* Todos here is a set of object of each todo with text, completionstatus and id */}
-                {/* Whereas todo is a single object that is mapped from todos */}
                 {filteredTodos.map(todo => (
                     <Todo
                         todo={todo}
