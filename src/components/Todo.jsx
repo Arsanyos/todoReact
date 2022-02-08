498248bea19f26dbddc0b22e6ab71bd990afbb51
Benat <abenezermarkos14@gmail.com>
2022-02-08 11:10:40 +0300

    Todo.jsx merge conflict resolved

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 
-<<<<<<< Updated upstream
-const Todo = ({ text, todo, todos, setTodos }) => {
-<<<<<<< Updated upstream
-=======
- const deleteHandler = (e) => {
-        //it deletes when the el.id is equal to the todo id(WHEN THE CONDITION IS FALSE)
-        setTodos(todos.filter((el) => el.id !== todo.id));
-    }
->>>>>>> Stashed changes
-=======
+
 //known as arrow function
 const Todo = ({ text, todo, todos, setTodos }) => {
     //events
@@ -17,7 +8,7 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         //it deletes when the el.id is equal to the todo id(WHEN THE CONDITION IS FALSE)
         setTodos(todos.filter((el) => el.id !== todo.id));
     }
->>>>>>> Stashed changes
+
     const completeHandler = () => {
         setTodos(todos.map((item) => {
             if (item.id === todo.id) {
@@ -28,26 +19,8 @@ const Todo = ({ text, todo, todos, setTodos }) => {
             return item;
         }))
     }
-<<<<<<< Updated upstream
- 
-    return (
-        <div className="todo">
-            <li className="todo container"}>{text}</li>
-            <button onClick={completeHandler} className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
-<<<<<<< Updated upstream
-=======
-             <button onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
-            </button>
->>>>>>> Stashed changes
-           
-        </div>
-    );
-}
-export default Todo;
-=======
+
+
     return (
         <div className="todo">
             <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
@@ -61,4 +34,4 @@ export default Todo;
     );
 }
 export default Todo;
->>>>>>> Stashed changes
+
